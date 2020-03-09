50c610af03a0c3852eea7d0492c84f59ba2b7868
Eszter Szluka <szluka.eszter@gmail.com>
2020-03-09 15:34:20 +0100

    'add restTemplate, Calendar'

--- calendar/src/main/java/com/codecool/postify/calendar/CalendarApplication.java
+++ calendar/src/main/java/com/codecool/postify/calendar/CalendarApplication.java
@@ -2,12 +2,31 @@ package com.codecool.postify.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Calendar;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
 public class CalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalendarApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public Calendar Calendar(){
+		return Calendar.getInstance();
+	}
 }
