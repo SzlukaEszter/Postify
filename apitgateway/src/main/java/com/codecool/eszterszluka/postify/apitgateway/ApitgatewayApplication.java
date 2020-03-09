c5b4e81b58f2dc6e444b9ba5e63d82125b83f396
Eszter Szluka <szluka.eszter@gmail.com>
2020-03-09 10:04:38 +0100

    'add gateway module'

---
+++ apitgateway/src/main/java/com/codecool/eszterszluka/postify/apitgateway/ApitgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.eszterszluka.postify.apitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitgatewayApplication.class, args);
+	}
+
+}
---
+++ apitgateway/src/test/java/com/codecool/eszterszluka/postify/apitgateway/ApitgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.eszterszluka.postify.apitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
